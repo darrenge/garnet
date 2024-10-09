81ee0abcc6de6b7b82882d66ae3a003f52668dee
github-action-benchmark <github@users.noreply.github.com>
2024-10-09 21:58:00 +0000

    add Benchmark (benchmarkdotnet) benchmark result for e70459e2209c0054a6369aa4b408cfc5efca4ed6

---
+++ dev/bench/data.js
@@ -0,0 +1,48 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1728511080035,
+  "repoUrl": "https://github.com/darrenge/garnet",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "name": "darrenge",
+            "username": "darrenge",
+            "email": "darrenge@microsoft.com"
+          },
+          "committer": {
+            "name": "darrenge",
+            "username": "darrenge",
+            "email": "darrenge@microsoft.com"
+          },
+          "id": "e70459e2209c0054a6369aa4b408cfc5efca4ed6",
+          "message": "Separate commit comment and chart into two separate tasks",
+          "timestamp": "2024-10-09T21:54:37Z",
+          "url": "https://github.com/darrenge/garnet/commit/e70459e2209c0054a6369aa4b408cfc5efca4ed6"
+        },
+        "date": 1728511079459,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "BDN.benchmark.Resp.RespTsavoriteStress.Get",
+            "value": 20693.0463470459,
+            "unit": "ns",
+            "range": "± 139.39975358550615"
+          },
+          {
+            "name": "BDN.benchmark.Resp.RespTsavoriteStress.Set",
+            "value": 18696.060368855793,
+            "unit": "ns",
+            "range": "± 173.56294797553468"
+          },
+          {
+            "name": "BDN.benchmark.Resp.RespTsavoriteStress.Incr",
+            "value": 26629.41668294271,
+            "unit": "ns",
+            "range": "± 153.0659715275589"
+          }
+        ]
+      }
+    ]
+  }
+}
