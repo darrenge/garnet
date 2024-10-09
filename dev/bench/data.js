85ac3108e8b885c92bd2dd9fd1f1a5295c149956
github-action-benchmark <github@users.noreply.github.com>
2024-10-09 21:58:21 +0000

    add Benchmark (benchmarkdotnet) benchmark result for e70459e2209c0054a6369aa4b408cfc5efca4ed6

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1728511080035,
+  "lastUpdate": 1728511101436,
   "repoUrl": "https://github.com/darrenge/garnet",
   "entries": {
     "Benchmark": [
@@ -40,6 +40,52 @@ window.BENCHMARK_DATA = {
             "value": 26629.41668294271,
             "unit": "ns",
             "range": "± 153.0659715275589"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "darrenge",
+            "username": "darrenge",
+            "email": "darrenge@microsoft.com"
+          },
+          "committer": {
+            "name": "darrenge",
+            "username": "darrenge",
+            "email": "darrenge@microsoft.com"
+          },
+          "id": "e70459e2209c0054a6369aa4b408cfc5efca4ed6",
+          "message": "Separate commit comment and chart into two separate tasks",
+          "timestamp": "2024-10-09T21:54:37Z",
+          "url": "https://github.com/darrenge/garnet/commit/e70459e2209c0054a6369aa4b408cfc5efca4ed6"
+        },
+        "date": 1728511100818,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "BDN.benchmark.Resp.RespLuaRunnerStress.BasicLuaRunner1",
+            "value": 199.0428880214691,
+            "unit": "ns",
+            "range": "± 0.9815463993518282"
+          },
+          {
+            "name": "BDN.benchmark.Resp.RespLuaRunnerStress.BasicLuaRunner2",
+            "value": 320.84334332148234,
+            "unit": "ns",
+            "range": "± 3.843941216278449"
+          },
+          {
+            "name": "BDN.benchmark.Resp.RespLuaRunnerStress.BasicLuaRunner3",
+            "value": 617.9448956171672,
+            "unit": "ns",
+            "range": "± 6.5630648211484965"
+          },
+          {
+            "name": "BDN.benchmark.Resp.RespLuaRunnerStress.BasicLuaRunner4",
+            "value": 2958.6016288757323,
+            "unit": "ns",
+            "range": "± 29.559345658825396"
           }
         ]
       }
