e4c68e2cf9d81284cda22b4e7058b41ba98eaabc
github-action-benchmark <github@users.noreply.github.com>
2024-10-17 21:16:00 +0000

    add Cluster.RespClusterMigrateBench (ubuntu-latest  net8.0 Release) (benchmarkdotnet) benchmark result for 24512dab706657aea425979fe01f7ede0572677d

--- website/static/charts/data.js
+++ website/static/charts/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1729199758748,
+  "lastUpdate": 1729199760186,
   "repoUrl": "https://github.com/darrenge/garnet",
   "entries": {
     "Benchmark": [
@@ -128,6 +128,54 @@ window.BENCHMARK_DATA = {
             "value": 28063.990509033203,
             "unit": "ns",
             "range": "± 182.33753260414232"
+          }
+        ]
+      }
+    ],
+    "Cluster.RespClusterMigrateBench (ubuntu-latest  net8.0 Release)": [
+      {
+        "commit": {
+          "author": {
+            "name": "darrenge",
+            "username": "darrenge",
+            "email": "darrenge@microsoft.com"
+          },
+          "committer": {
+            "name": "darrenge",
+            "username": "darrenge",
+            "email": "darrenge@microsoft.com"
+          },
+          "id": "24512dab706657aea425979fe01f7ede0572677d",
+          "message": "Put back full tests etc",
+          "timestamp": "2024-10-17T21:11:59Z",
+          "url": "https://github.com/darrenge/garnet/commit/24512dab706657aea425979fe01f7ede0572677d"
+        },
+        "date": 1729199759767,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "BDN.benchmark.Cluster.RespClusterMigrateBench.Get",
+            "value": 47730.66461995443,
+            "unit": "ns",
+            "range": "± 239.4879000166457"
+          },
+          {
+            "name": "BDN.benchmark.Cluster.RespClusterMigrateBench.Set",
+            "value": 47779.88855489095,
+            "unit": "ns",
+            "range": "± 59.96728146783521"
+          },
+          {
+            "name": "BDN.benchmark.Cluster.RespClusterMigrateBench.MGet",
+            "value": 40499.21034545898,
+            "unit": "ns",
+            "range": "± 250.99137932928903"
+          },
+          {
+            "name": "BDN.benchmark.Cluster.RespClusterMigrateBench.MSet",
+            "value": 39338.27721698467,
+            "unit": "ns",
+            "range": "± 52.14553980262812"
           }
         ]
       }
