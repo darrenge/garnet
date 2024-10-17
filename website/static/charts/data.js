28f1125b8419e3a36aaedcc50be09c8f25928160
github-action-benchmark <github@users.noreply.github.com>
2024-10-17 21:15:55 +0000

    add Resp.RespTsavoriteStress (ubuntu-latest  net8.0 Release) (benchmarkdotnet) benchmark result for 24512dab706657aea425979fe01f7ede0572677d

--- website/static/charts/data.js
+++ website/static/charts/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1729199753418,
+  "lastUpdate": 1729199755313,
   "repoUrl": "https://github.com/darrenge/garnet",
   "entries": {
     "Benchmark": [
@@ -21,6 +21,48 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/darrenge/garnet/commit/24512dab706657aea425979fe01f7ede0572677d"
         },
         "date": 1729199751684,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "BDN.benchmark.Resp.RespTsavoriteStress.Get",
+            "value": 20761.653611246744,
+            "unit": "ns",
+            "range": "± 367.2510752455851"
+          },
+          {
+            "name": "BDN.benchmark.Resp.RespTsavoriteStress.Set",
+            "value": 19149.303718566895,
+            "unit": "ns",
+            "range": "± 79.2057754346537"
+          },
+          {
+            "name": "BDN.benchmark.Resp.RespTsavoriteStress.Incr",
+            "value": 28063.990509033203,
+            "unit": "ns",
+            "range": "± 182.33753260414232"
+          }
+        ]
+      }
+    ],
+    "Resp.RespTsavoriteStress (ubuntu-latest  net8.0 Release)": [
+      {
+        "commit": {
+          "author": {
+            "name": "darrenge",
+            "username": "darrenge",
+            "email": "darrenge@microsoft.com"
+          },
+          "committer": {
+            "name": "darrenge",
+            "username": "darrenge",
+            "email": "darrenge@microsoft.com"
+          },
+          "id": "24512dab706657aea425979fe01f7ede0572677d",
+          "message": "Put back full tests etc",
+          "timestamp": "2024-10-17T21:11:59Z",
+          "url": "https://github.com/darrenge/garnet/commit/24512dab706657aea425979fe01f7ede0572677d"
+        },
+        "date": 1729199754735,
         "tool": "benchmarkdotnet",
         "benches": [
           {
