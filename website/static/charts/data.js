5e10bc0fdc0fe48cd6f887c890c6dc9d6ba0c643
github-action-benchmark <github@users.noreply.github.com>
2024-10-17 23:01:10 +0000

    add Benchmark (benchmarkdotnet) benchmark result for 143387b14e3a21269750b7e1e8423fc17426b211

--- website/static/charts/data.js
+++ website/static/charts/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1729206060572,
+  "lastUpdate": 1729206070732,
   "repoUrl": "https://github.com/darrenge/garnet",
   "entries": {
     "Benchmark": [
@@ -1754,6 +1754,46 @@ window.BENCHMARK_DATA = {
             "value": 235752.43994140625,
             "unit": "ns",
             "range": "± 1068.5373753434103"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "darrenge",
+            "username": "darrenge",
+            "email": "darrenge@microsoft.com"
+          },
+          "committer": {
+            "name": "darrenge",
+            "username": "darrenge",
+            "email": "darrenge@microsoft.com"
+          },
+          "id": "143387b14e3a21269750b7e1e8423fc17426b211",
+          "message": "Set continue on fail to false so if allocated is too high it won't run the others",
+          "timestamp": "2024-10-17T22:58:02Z",
+          "url": "https://github.com/darrenge/garnet/commit/143387b14e3a21269750b7e1e8423fc17426b211"
+        },
+        "date": 1729206069578,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "BDN.benchmark.Resp.RespTsavoriteStress.Get",
+            "value": 20473.964698791504,
+            "unit": "ns",
+            "range": "± 24.00415880180974"
+          },
+          {
+            "name": "BDN.benchmark.Resp.RespTsavoriteStress.Set",
+            "value": 19694.95630318778,
+            "unit": "ns",
+            "range": "± 151.0059786321517"
+          },
+          {
+            "name": "BDN.benchmark.Resp.RespTsavoriteStress.Incr",
+            "value": 25717.599790300643,
+            "unit": "ns",
+            "range": "± 197.08385663521423"
           }
         ]
       }
