9aedcc8f98d1e31df52655dc8238ced65fc22a02
github-action-benchmark <github@users.noreply.github.com>
2024-10-17 22:50:15 +0000

    add Resp.RespTsavoriteStress (windows-latest  net8.0 Release) (benchmarkdotnet) benchmark result for eeb13f78d759f5b048dfadd78e9a9f3f10b55012

--- website/static/charts/data.js
+++ website/static/charts/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1729205411443,
+  "lastUpdate": 1729205414930,
   "repoUrl": "https://github.com/darrenge/garnet",
   "entries": {
     "Benchmark": [
@@ -1376,6 +1376,46 @@ window.BENCHMARK_DATA = {
             "value": 25449.247272198016,
             "unit": "ns",
             "range": "± 30.600102066234513"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "darrenge",
+            "username": "darrenge",
+            "email": "darrenge@microsoft.com"
+          },
+          "committer": {
+            "name": "darrenge",
+            "username": "darrenge",
+            "email": "darrenge@microsoft.com"
+          },
+          "id": "eeb13f78d759f5b048dfadd78e9a9f3f10b55012",
+          "message": "Removed checks for mean and all expected",
+          "timestamp": "2024-10-17T22:45:10Z",
+          "url": "https://github.com/darrenge/garnet/commit/eeb13f78d759f5b048dfadd78e9a9f3f10b55012"
+        },
+        "date": 1729205413488,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "BDN.benchmark.Resp.RespTsavoriteStress.Get",
+            "value": 21834.562247140067,
+            "unit": "ns",
+            "range": "± 65.05153821041347"
+          },
+          {
+            "name": "BDN.benchmark.Resp.RespTsavoriteStress.Set",
+            "value": 17783.23495047433,
+            "unit": "ns",
+            "range": "± 19.999442460421374"
+          },
+          {
+            "name": "BDN.benchmark.Resp.RespTsavoriteStress.Incr",
+            "value": 26417.130807729867,
+            "unit": "ns",
+            "range": "± 15.275915196816419"
           }
         ]
       }
