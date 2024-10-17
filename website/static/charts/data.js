2725ba50de18015fb2b5b8e91b930bc38a40d9f6
github-action-benchmark <github@users.noreply.github.com>
2024-10-17 22:58:18 +0000

    add Cluster.RespClusterMigrateBench (ubuntu-latest  net8.0 Release) (benchmarkdotnet) benchmark result for 143387b14e3a21269750b7e1e8423fc17426b211

--- website/static/charts/data.js
+++ website/static/charts/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1729205897458,
+  "lastUpdate": 1729205898762,
   "repoUrl": "https://github.com/darrenge/garnet",
   "entries": {
     "Benchmark": [
@@ -1526,6 +1526,52 @@ window.BENCHMARK_DATA = {
             "value": 42044.22091878255,
             "unit": "ns",
             "range": "± 255.25874859511967"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "darrenge",
+            "username": "darrenge",
+            "email": "darrenge@microsoft.com"
+          },
+          "committer": {
+            "name": "darrenge",
+            "username": "darrenge",
+            "email": "darrenge@microsoft.com"
+          },
+          "id": "143387b14e3a21269750b7e1e8423fc17426b211",
+          "message": "Set continue on fail to false so if allocated is too high it won't run the others",
+          "timestamp": "2024-10-17T22:58:02Z",
+          "url": "https://github.com/darrenge/garnet/commit/143387b14e3a21269750b7e1e8423fc17426b211"
+        },
+        "date": 1729205898441,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "BDN.benchmark.Cluster.RespClusterMigrateBench.Get",
+            "value": 47127.161313883465,
+            "unit": "ns",
+            "range": "± 455.97857848195537"
+          },
+          {
+            "name": "BDN.benchmark.Cluster.RespClusterMigrateBench.Set",
+            "value": 47858.10873413086,
+            "unit": "ns",
+            "range": "± 73.38150103518961"
+          },
+          {
+            "name": "BDN.benchmark.Cluster.RespClusterMigrateBench.MGet",
+            "value": 42364.10197666713,
+            "unit": "ns",
+            "range": "± 225.54883609685052"
+          },
+          {
+            "name": "BDN.benchmark.Cluster.RespClusterMigrateBench.MSet",
+            "value": 39904.964430588945,
+            "unit": "ns",
+            "range": "± 117.87057276679414"
           }
         ]
       }
