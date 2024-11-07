2cf816f4eab1b5c32536cbaeb2525c8af7b87654
github-action-benchmark <github@users.noreply.github.com>
2024-11-07 18:21:51 +0000

    add Operations.ObjectOperations (windows-latest  net8.0 Release) (benchmarkdotnet) benchmark result for 550eea9af1035b076d95ea45fe8d7609aa7ca6b1

--- website/static/charts/data.js
+++ website/static/charts/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1731003707442,
+  "lastUpdate": 1731003711101,
   "repoUrl": "https://github.com/darrenge/garnet",
   "entries": {
     "Benchmark": [
@@ -21538,6 +21538,102 @@ window.BENCHMARK_DATA = {
             "value": 120459.04215494792,
             "unit": "ns",
             "range": "± 378.6357113672217"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "darrenge@microsoft.com",
+            "name": "darrenge",
+            "username": "darrenge"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "550eea9af1035b076d95ea45fe8d7609aa7ca6b1",
+          "message": "Merge branch 'microsoft:main' into main",
+          "timestamp": "2024-11-07T10:12:32-08:00",
+          "tree_id": "ee79e644475534f08e9a1b040fdde13f087ac6e1",
+          "url": "https://github.com/darrenge/garnet/commit/550eea9af1035b076d95ea45fe8d7609aa7ca6b1"
+        },
+        "date": 1731003709404,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "BDN.benchmark.Operations.ObjectOperations.ZAddRem(Params: ACL)",
+            "value": 116120.0370279948,
+            "unit": "ns",
+            "range": "± 255.68107750967926"
+          },
+          {
+            "name": "BDN.benchmark.Operations.ObjectOperations.LPushPop(Params: ACL)",
+            "value": 99782.69391741071,
+            "unit": "ns",
+            "range": "± 268.95801240492943"
+          },
+          {
+            "name": "BDN.benchmark.Operations.ObjectOperations.SAddRem(Params: ACL)",
+            "value": 98186.79460797991,
+            "unit": "ns",
+            "range": "± 237.6151673158719"
+          },
+          {
+            "name": "BDN.benchmark.Operations.ObjectOperations.HSetDel(Params: ACL)",
+            "value": 141417.60777064733,
+            "unit": "ns",
+            "range": "± 760.0308327962892"
+          },
+          {
+            "name": "BDN.benchmark.Operations.ObjectOperations.ZAddRem(Params: AOF)",
+            "value": 133109.44998604912,
+            "unit": "ns",
+            "range": "± 655.0180450911867"
+          },
+          {
+            "name": "BDN.benchmark.Operations.ObjectOperations.LPushPop(Params: AOF)",
+            "value": 111807.86568777902,
+            "unit": "ns",
+            "range": "± 495.68388390141604"
+          },
+          {
+            "name": "BDN.benchmark.Operations.ObjectOperations.SAddRem(Params: AOF)",
+            "value": 110295.15380859375,
+            "unit": "ns",
+            "range": "± 285.37761442792737"
+          },
+          {
+            "name": "BDN.benchmark.Operations.ObjectOperations.HSetDel(Params: AOF)",
+            "value": 134477.3228236607,
+            "unit": "ns",
+            "range": "± 653.1348795704939"
+          },
+          {
+            "name": "BDN.benchmark.Operations.ObjectOperations.ZAddRem(Params: None)",
+            "value": 117427.82418387277,
+            "unit": "ns",
+            "range": "± 298.5104945485388"
+          },
+          {
+            "name": "BDN.benchmark.Operations.ObjectOperations.LPushPop(Params: None)",
+            "value": 99846.5055025541,
+            "unit": "ns",
+            "range": "± 161.73399733214134"
+          },
+          {
+            "name": "BDN.benchmark.Operations.ObjectOperations.SAddRem(Params: None)",
+            "value": 105335.63319614956,
+            "unit": "ns",
+            "range": "± 182.695973941013"
+          },
+          {
+            "name": "BDN.benchmark.Operations.ObjectOperations.HSetDel(Params: None)",
+            "value": 123127.06217447917,
+            "unit": "ns",
+            "range": "± 520.4138028245181"
           }
         ]
       }
