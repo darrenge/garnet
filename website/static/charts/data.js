774d41aaf5aa6b068e30c09f21e1f849a9b36eb2
github-action-benchmark <github@users.noreply.github.com>
2024-11-25 18:56:42 +0000

    add Benchmark (benchmarkdotnet) benchmark result for 4720de5e773d9df5262bec47a81c4748839740db

--- website/static/charts/data.js
+++ website/static/charts/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1732561001845,
+  "lastUpdate": 1732561002914,
   "repoUrl": "https://github.com/darrenge/garnet",
   "entries": {
     "Benchmark": [
@@ -12514,6 +12514,54 @@ window.BENCHMARK_DATA = {
             "value": 1750.9648040771485,
             "unit": "ns",
             "range": "± 14.214152856726143"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "darrenge@microsoft.com",
+            "name": "darrenge",
+            "username": "darrenge"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "4720de5e773d9df5262bec47a81c4748839740db",
+          "message": "Merge branch 'microsoft:main' into main",
+          "timestamp": "2024-11-25T10:53:33-08:00",
+          "tree_id": "895acf932575ac7e2f57fe569161c01f3dd8fa98",
+          "url": "https://github.com/darrenge/garnet/commit/4720de5e773d9df5262bec47a81c4748839740db"
+        },
+        "date": 1732561000208,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "BDN.benchmark.Cluster.ClusterMigrate.Get(Params: None)",
+            "value": 37542.51149698893,
+            "unit": "ns",
+            "range": "± 253.70939586959716"
+          },
+          {
+            "name": "BDN.benchmark.Cluster.ClusterMigrate.Set(Params: None)",
+            "value": 37694.17896728516,
+            "unit": "ns",
+            "range": "± 382.62098727643735"
+          },
+          {
+            "name": "BDN.benchmark.Cluster.ClusterMigrate.MGet(Params: None)",
+            "value": 32035.79453531901,
+            "unit": "ns",
+            "range": "± 257.64461643563084"
+          },
+          {
+            "name": "BDN.benchmark.Cluster.ClusterMigrate.MSet(Params: None)",
+            "value": 31908.640881347656,
+            "unit": "ns",
+            "range": "± 293.1259259806305"
           }
         ]
       }
