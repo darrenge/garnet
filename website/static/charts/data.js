4a7eb66a68da51058bd4b090602071a9b76b888a
github-action-benchmark <github@users.noreply.github.com>
2024-11-26 23:41:33 +0000

    add Benchmark (benchmarkdotnet) benchmark result for a6ed3a1c5378f4b81f6de19b8eafeb0698c6a3c2

--- website/static/charts/data.js
+++ website/static/charts/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1732664396651,
+  "lastUpdate": 1732664493147,
   "repoUrl": "https://github.com/darrenge/garnet",
   "entries": {
     "Benchmark": [
@@ -13246,6 +13246,48 @@ window.BENCHMARK_DATA = {
             "value": 2969.24182027181,
             "unit": "ns",
             "range": "± 17.944916235621424"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "darrenge@microsoft.com",
+            "name": "darrenge",
+            "username": "darrenge"
+          },
+          "committer": {
+            "email": "darrenge@microsoft.com",
+            "name": "darrenge",
+            "username": "darrenge"
+          },
+          "distinct": true,
+          "id": "a6ed3a1c5378f4b81f6de19b8eafeb0698c6a3c2",
+          "message": "Added BDN.benchmark to beginning for test if not needed",
+          "timestamp": "2024-11-26T15:36:29-08:00",
+          "tree_id": "69d67643d8f2f9c5254036cffea0629aedef5f33",
+          "url": "https://github.com/darrenge/garnet/commit/a6ed3a1c5378f4b81f6de19b8eafeb0698c6a3c2"
+        },
+        "date": 1732664484157,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "BDN.benchmark.Operations.BasicOperations.InlinePing(Params: ACL)",
+            "value": 1799.615904490153,
+            "unit": "ns",
+            "range": "± 3.585605653947827"
+          },
+          {
+            "name": "BDN.benchmark.Operations.BasicOperations.InlinePing(Params: AOF)",
+            "value": 1800.3263253432053,
+            "unit": "ns",
+            "range": "± 2.29445331714014"
+          },
+          {
+            "name": "BDN.benchmark.Operations.BasicOperations.InlinePing(Params: None)",
+            "value": 1785.2965899876185,
+            "unit": "ns",
+            "range": "± 23.607186711467016"
           }
         ]
       }
