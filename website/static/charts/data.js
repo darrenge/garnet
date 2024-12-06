50d130f4d698781de35a8887bc6d88c3a6065553
github-action-benchmark <github@users.noreply.github.com>
2024-12-06 23:16:11 +0000

    add Operations.BasicOperations (ubuntu-latest  net8.0 Release) (benchmarkdotnet) benchmark result for d14ca37c765799021a3e84fcc669ab56f57e4892

--- website/static/charts/data.js
+++ website/static/charts/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1733526969107,
+  "lastUpdate": 1733526971024,
   "repoUrl": "https://github.com/darrenge/garnet",
   "entries": {
     "Benchmark": [
@@ -24786,6 +24786,48 @@ window.BENCHMARK_DATA = {
             "value": 1750.975915636335,
             "unit": "ns",
             "range": "± 14.385225325031739"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "darrenge@microsoft.com",
+            "name": "darrenge",
+            "username": "darrenge"
+          },
+          "committer": {
+            "email": "darrenge@microsoft.com",
+            "name": "darrenge",
+            "username": "darrenge"
+          },
+          "distinct": true,
+          "id": "d14ca37c765799021a3e84fcc669ab56f57e4892",
+          "message": "Trying to add specific ref in the check out",
+          "timestamp": "2024-12-06T15:12:45-08:00",
+          "tree_id": "5fad5a3cd52db389fe023a6a7bb733d3b8469bbe",
+          "url": "https://github.com/darrenge/garnet/commit/d14ca37c765799021a3e84fcc669ab56f57e4892"
+        },
+        "date": 1733526970229,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "BDN.benchmark.Operations.BasicOperations.InlinePing(Params: ACL)",
+            "value": 1841.811187199184,
+            "unit": "ns",
+            "range": "± 14.561917113835829"
+          },
+          {
+            "name": "BDN.benchmark.Operations.BasicOperations.InlinePing(Params: AOF)",
+            "value": 1745.279776763916,
+            "unit": "ns",
+            "range": "± 14.61271074816318"
+          },
+          {
+            "name": "BDN.benchmark.Operations.BasicOperations.InlinePing(Params: None)",
+            "value": 1746.250542195638,
+            "unit": "ns",
+            "range": "± 15.208308969299893"
           }
         ]
       }
