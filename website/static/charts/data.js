d4a7c65c473675c929d7ec8af649cf8fc3c7cd2a
github-action-benchmark <github@users.noreply.github.com>
2024-12-10 17:49:58 +0000

    add Lua.LuaScripts (ubuntu-latest  net8.0 Release) (benchmarkdotnet) benchmark result for a8abb29c4520d9982abd24e00c0724b9a89afadd

--- website/static/charts/data.js
+++ website/static/charts/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1733852994351,
+  "lastUpdate": 1733852998295,
   "repoUrl": "https://github.com/darrenge/garnet",
   "entries": {
     "Benchmark": [
@@ -63234,6 +63234,54 @@ window.BENCHMARK_DATA = {
             "value": 3033.3772945404053,
             "unit": "ns",
             "range": "± 25.41447264601413"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "darrenge@microsoft.com",
+            "name": "darrenge",
+            "username": "darrenge"
+          },
+          "committer": {
+            "email": "darrenge@microsoft.com",
+            "name": "darrenge",
+            "username": "darrenge"
+          },
+          "distinct": true,
+          "id": "a8abb29c4520d9982abd24e00c0724b9a89afadd",
+          "message": "Trying again",
+          "timestamp": "2024-12-10T09:45:57-08:00",
+          "tree_id": "fd68ad548d906568c40ac5d15f55df9eca17b7c4",
+          "url": "https://github.com/darrenge/garnet/commit/a8abb29c4520d9982abd24e00c0724b9a89afadd"
+        },
+        "date": 1733852997856,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "BDN.benchmark.Lua.LuaScripts.Script1(Params: None)",
+            "value": 181.31071996688843,
+            "unit": "ns",
+            "range": "± 1.0997980229572795"
+          },
+          {
+            "name": "BDN.benchmark.Lua.LuaScripts.Script2(Params: None)",
+            "value": 307.52278372446693,
+            "unit": "ns",
+            "range": "± 2.326887574559167"
+          },
+          {
+            "name": "BDN.benchmark.Lua.LuaScripts.Script3(Params: None)",
+            "value": 591.9691880544027,
+            "unit": "ns",
+            "range": "± 4.6876511349523495"
+          },
+          {
+            "name": "BDN.benchmark.Lua.LuaScripts.Script4(Params: None)",
+            "value": 2985.4909379141673,
+            "unit": "ns",
+            "range": "± 25.432125374054664"
           }
         ]
       }
