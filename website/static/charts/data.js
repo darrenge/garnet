a2221aca04e1bbaaee60bdfdf8c8256edb5e40e1
github-action-benchmark <github@users.noreply.github.com>
2024-12-10 18:49:44 +0000

    add Benchmark (benchmarkdotnet) benchmark result for b82ab394098049f65617b7ec819212444b6eb49f

--- website/static/charts/data.js
+++ website/static/charts/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1733856579252,
+  "lastUpdate": 1733856583943,
   "repoUrl": "https://github.com/darrenge/garnet",
   "entries": {
     "Benchmark": [
@@ -35034,6 +35034,48 @@ window.BENCHMARK_DATA = {
             "value": 32057.862774658202,
             "unit": "ns",
             "range": "± 284.6618153466449"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "darrenge@microsoft.com",
+            "name": "darrenge",
+            "username": "darrenge"
+          },
+          "committer": {
+            "email": "darrenge@microsoft.com",
+            "name": "darrenge",
+            "username": "darrenge"
+          },
+          "distinct": true,
+          "id": "b82ab394098049f65617b7ec819212444b6eb49f",
+          "message": "another try",
+          "timestamp": "2024-12-10T10:46:27-08:00",
+          "tree_id": "6a5dda048ed4884efff4dba977d4f61218149684",
+          "url": "https://github.com/darrenge/garnet/commit/b82ab394098049f65617b7ec819212444b6eb49f"
+        },
+        "date": 1733856580826,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "BDN.benchmark.Operations.BasicOperations.InlinePing(Params: ACL)",
+            "value": 1771.2102549235026,
+            "unit": "ns",
+            "range": "± 12.712738995547694"
+          },
+          {
+            "name": "BDN.benchmark.Operations.BasicOperations.InlinePing(Params: AOF)",
+            "value": 1734.1254746119182,
+            "unit": "ns",
+            "range": "± 1.5611526476188031"
+          },
+          {
+            "name": "BDN.benchmark.Operations.BasicOperations.InlinePing(Params: None)",
+            "value": 1732.60311683019,
+            "unit": "ns",
+            "range": "± 1.0132988005698793"
           }
         ]
       }
