2e057e90a6cce0dbbc7fb3237bc8349e42164568
github-action-benchmark <github@users.noreply.github.com>
2025-04-22 16:21:37 +0000

    add Network.BasicOperations (ubuntu-latest  net8.0 Release) (benchmarkdotnet) benchmark result for ff40f08003da7409e329b455acb1e8805e3616e9

--- website/static/charts/data.js
+++ website/static/charts/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1745338884021,
+  "lastUpdate": 1745338897659,
   "repoUrl": "https://github.com/darrenge/garnet",
   "entries": {
     "Network.BasicOperations (ubuntu-latest  net9.0 Release)": [
@@ -30,6 +30,38 @@ window.BENCHMARK_DATA = {
             "value": 83.11810561418534,
             "unit": "ns",
             "range": "± 0.42326322381170567"
+          }
+        ]
+      }
+    ],
+    "Network.BasicOperations (ubuntu-latest  net8.0 Release)": [
+      {
+        "commit": {
+          "author": {
+            "email": "darrenge@microsoft.com",
+            "name": "darrenge",
+            "username": "darrenge"
+          },
+          "committer": {
+            "email": "darrenge@microsoft.com",
+            "name": "darrenge",
+            "username": "darrenge"
+          },
+          "distinct": true,
+          "id": "ff40f08003da7409e329b455acb1e8805e3616e9",
+          "message": "Updated to main",
+          "timestamp": "2025-04-22T09:18:00-07:00",
+          "tree_id": "1697ee5b26ded92e3d98070bacca54bc950e95c7",
+          "url": "https://github.com/darrenge/garnet/commit/ff40f08003da7409e329b455acb1e8805e3616e9"
+        },
+        "date": 1745338890841,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "BDN.benchmark.Network.BasicOperations.InlinePing(Params: None)",
+            "value": 87.03614935477574,
+            "unit": "ns",
+            "range": "± 0.376945503248046"
           }
         ]
       }
